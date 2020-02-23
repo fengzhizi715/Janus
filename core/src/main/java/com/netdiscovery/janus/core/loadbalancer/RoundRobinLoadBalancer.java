9c20955c3e9414e39277bbb2ebdbf058a26b3600
bdqsky <bdqsky@163.com>
2020-02-23 21:34:51 +0800

    fix bug of Multi-threading

--- core/src/main/java/com/netdiscovery/janus/core/loadbalancer/RoundRobinLoadBalancer.java
+++ core/src/main/java/com/netdiscovery/janus/core/loadbalancer/RoundRobinLoadBalancer.java
@@ -3,19 +3,19 @@ package com.netdiscovery.janus.core.loadbalancer;
 import com.netdiscovery.janus.core.endpoint.Endpoint;
 
 import java.util.List;
-import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * @author bdq
  * @since 2020/1/14
  */
 public class RoundRobinLoadBalancer extends LoadBalancer {
-    private static final AtomicInteger INDEX = new AtomicInteger(0);
+    private static final ThreadLocal<Integer> COUNT = ThreadLocal.withInitial(() -> 0);
 
     @Override
     protected Endpoint doLoadBalance(List<Endpoint> availables) {
         int size = availables.size();
-        int index = INDEX.getAndIncrement();
+        int index = COUNT.get() + 1;
+        COUNT.set(index);
         return availables.get(index % size);
     }
 }
